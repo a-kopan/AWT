c4ab55987fc5692b1281dcd1519e4ce9982eee94
Kopan <aakk123221@gmail.com>
2025-04-07 23:35:10 +0200

    Added vue template

---
+++ Lab6/address-book-vue-app/src/components/PersonsTable.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="persons-table">
+        <table>
+            <tbody>
+                <tr v-for="person in personsSource" :key="person.id">
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.email }}</td>
+                    <td>{{ person.phone }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'persons-table',
+        props: {
+            personsSource: Array
+        },
+    }
+    
+</script>
+
+<style scoped></style>
